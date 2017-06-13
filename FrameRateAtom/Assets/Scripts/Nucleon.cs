e67048abd5f60d0d772a249126b69fd0fde2ad10
Julian Nicholls <jnicholls95@outlook.com>
2017-06-12 22:08:47 -0400

    Finished Product

---
+++ FrameRateAtom/Assets/Scripts/Nucleon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ensure a rigidbody is attached
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour {
+
+	public float attractionForce;
+	Rigidbody body;
+
+	// Use this for initialization
+	//not sure why we're using awake instead of start
+	void Awake () {
+		body = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	//fixed update is used for physics so it's not dependent on frames
+	void FixedUpdate () {
+
+		//not sure how this makes them all collide in the centre, but it does
+		body.AddForce (transform.localPosition * -attractionForce);
+	}
+}
---
+++ FrameRateAtom/Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+
+	//public access to edit from editor
+	public float timeBetweenSpawns;
+	public float spawnDistance;
+	public Nucleon[] nucleonPrefabs;
+
+	float timeSinceLastSpawn;
+
+	// Use this for initialization
+	void Start () {
+
+		//with V Sync disabled, this is to prevent hardware overuse
+		Application.targetFrameRate = 70;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		//time since last prefab was spawned
+		timeSinceLastSpawn = timeSinceLastSpawn + Time.deltaTime;
+
+		//if enough time has elapsed, spawn prefab and reset time
+		if (timeSinceLastSpawn >= timeBetweenSpawns) {
+			timeSinceLastSpawn = timeSinceLastSpawn - timeBetweenSpawns;
+			SpawnNucleon ();
+		}
+	}
+
+	void SpawnNucleon(){
+
+		//choose one of two prefabs, and place it a set distance from the centre
+		Nucleon prefab = nucleonPrefabs [Random.Range (0, nucleonPrefabs.Length)];
+		Nucleon spawn = Instantiate<Nucleon> (prefab);
+		spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+	}
+}
